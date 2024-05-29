ef9c7033fea9e8581f3d45481f22f1fbc68c8a20
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-05-29 20:13:46 +0300

    Add Items Modal

--- src/components/shared/CalledComponents.tsx
+++ src/components/shared/CalledComponents.tsx
@@ -2,13 +2,12 @@ import { Stack, Divider, Typography, Paper } from '@mui/material'
 
 import { HeroImage, TestmonialsData, TestmonialsImage } from '../ui'
 import { StartBookingForm } from '../forms'
-import DeleteModal from '../modals/DeleteModal'
+import { DeleteModal, AddItemsModal } from '../modals/'
 
 const CalledComponents = () => {
   return (
     <>
       <Stack>
-        
         <Divider />
         <Paper sx={{ p: 2, mb: 2 }} elevation={3}>
           <Typography variant='h4' gutterBottom>
@@ -21,15 +20,20 @@ const CalledComponents = () => {
             <TestmonialsData />
           </Stack>
 
-          <Divider sx={{pt: 4}} />
-          <Typography variant='h4' gutterBottom sx={{pt:4}}>
+          <Divider sx={{ pt: 4 }} />
+          <Typography variant='h4' gutterBottom sx={{ pt: 4 }}>
             Modals
           </Typography>
           <Stack direction='row' spacing={8} sx={{ pt: 2 }}>
             <DeleteModal />
+            <AddItemsModal title='Add Items'>
+              <Typography variant='body1' gutterBottom>
+                Add your items here
+              </Typography>
+            </AddItemsModal>
           </Stack>
 
-          <Typography variant='h4' gutterBottom sx={{pt: 6}}>
+          <Typography variant='h4' gutterBottom sx={{ pt: 6 }}>
             Combined Elements
           </Typography>
         </Paper>
