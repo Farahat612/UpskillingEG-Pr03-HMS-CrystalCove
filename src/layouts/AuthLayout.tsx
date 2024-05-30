7aaaab59899b7f86affda9ad3dda18276c740f1d
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-05-30 08:22:28 +0300

    Authentication Layout Done

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,13 +1,20 @@
-import { Box, Grid } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 import Logo from '../assets/logo/logo.png'
 
 type AuthLayoutProps = {
   children: React.ReactNode
   imageSrc: string
+  imgTitle?: string
+  slogan?: string
 }
 
-const AuthLayout = ({ children, imageSrc }: AuthLayoutProps) => {
+const AuthLayout = ({
+  children,
+  imageSrc,
+  imgTitle,
+  slogan,
+}: AuthLayoutProps) => {
   return (
     <>
       <Grid component={'section'} container sx={{ height: '100vh', p: 3 }}>
@@ -41,19 +48,59 @@ const AuthLayout = ({ children, imageSrc }: AuthLayoutProps) => {
           </Box>
         </Grid>
         <Grid item xs={12} md={6} sx={{ height: { xs: '50%', md: '100%' } }}>
-          <Box sx={{ width: '100%', height: '100%' }}>
+          <Box
+            sx={{
+              width: '100%',
+              height: '100%',
+              borderRadius: '1rem',
+              position: 'relative',
+            }}
+          >
+            {/* Image */}
             <img
               src={imageSrc}
               alt='image'
               style={{
+                position: 'absolute',
                 height: '100%',
                 width: '100%',
                 maxWidth: '100%',
-                borderRadius: '1rem',
+                borderRadius: 'inherit',
                 objectFit: 'cover',
                 display: 'block',
               }}
             />
+            {/* Overlay */}
+            <Box
+              sx={{
+                position: 'absolute',
+                height: '100%',
+                width: '100%',
+                borderRadius: 'inherit',
+                background:
+                  'linear-gradient(to top, rgba(0,0,0,0.9), rgba(0,0,0,0.3))',
+              }}
+            />
+
+            {/* Slogan and Title */}
+            {imgTitle && slogan && (
+              <Box
+                sx={{
+                  color: 'white',
+                  position: 'absolute',
+                  zIndex: 10,
+                  bottom: '2rem',
+                  left: '2rem',
+                }}
+              >
+                <Typography variant='h6' fontSize={30}>
+                  {imgTitle}
+                </Typography>
+                <Typography variant='body1' fontSize={20}>
+                  {slogan}
+                </Typography>
+              </Box>
+            )}
           </Box>
         </Grid>
       </Grid>
--- src/pages/1.Authentication/LoginPage.tsx
+++ src/pages/1.Authentication/LoginPage.tsx
@@ -1,10 +1,14 @@
 import { AuthLayout } from '../../layouts'
-import SigninImage from '../../assets/images/Signin.png'
+import SigninImage from '../../assets/forms/sign-in.png'
 import { AuthForm } from '../../components/forms'
 
 const LoginPage = () => {
   return (
-    <AuthLayout imageSrc={SigninImage}>
+    <AuthLayout
+      imageSrc={SigninImage}
+      imgTitle='Sign In to CrystalCove'
+      slogan='Homes as unique as you.'
+    >
       <AuthForm
         heading='Login'
         paragraph='If you already have an account register You can'
