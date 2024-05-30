fcf8b90c22a9605d961f25ec6ac1910d63a31ad8
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-05-30 09:16:01 +0300

    Login Page Initial

--- src/pages/1.Authentication/LoginPage.tsx
+++ src/pages/1.Authentication/LoginPage.tsx
@@ -1,8 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { AuthLayout } from '../../layouts'
 import SigninImage from '../../assets/forms/sign-in.png'
 import { AuthForm } from '../../components/forms'
+import { Button, Stack, TextField, Typography } from '@mui/material'
+
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const LoginPage = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false)
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible)
+  }
   return (
     <AuthLayout
       imageSrc={SigninImage}
@@ -15,7 +24,60 @@ const LoginPage = () => {
         linkText='Register here!'
         linkDestination='/register'
       >
-        <h3>Login</h3>
+        <form>
+          <Stack spacing={3} sx={{ mt: 4 }}>
+            {/* Email */}
+            <Stack spacing={0}>
+              <Typography variant='subtitle2' color={'primary.main'}>
+                Email Address
+              </Typography>
+              <TextField
+                type='text'
+                variant='outlined'
+                fullWidth
+                size='small'
+                sx={{
+                  bgcolor: 'rgba(245, 246, 248, 1)',
+                }}
+              />
+            </Stack>
+            {/* Password */}
+            <Stack spacing={0}>
+              <Typography variant='subtitle2' color={'primary.main'}>
+                Password
+              </Typography>
+              <TextField
+                type={passwordVisible ? 'text' : 'password'}
+                variant='outlined'
+                fullWidth
+                size='small'
+                sx={{
+                  bgcolor: 'rgba(245, 246, 248, 1)',
+                }}
+              />
+              {/* Forgot Password Link */}
+              <Typography variant='subtitle2' sx={{ mt: 1, ml: 'auto' }}>
+                <Link
+                  to={'/forgot-password'}
+                  style={{ color: 'rgba(0, 0, 0, 0.54)' }}
+                >
+                  Forgot Password?
+                </Link>
+              </Typography>
+            </Stack>
+          </Stack>
+
+          {/* Submit Button */}
+          <Stack spacing={0} sx={{ mt: 3 }}>
+            <Button
+              type='submit'
+              className='btn btn-primary btn-block'
+              variant='contained'
+            >
+              Login
+            </Button>
+          </Stack>
+        </form>
       </AuthForm>
     </AuthLayout>
   )
