21431a006fbaf4be0a98e8ef4401067c66d05e77
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-05-30 07:12:01 +0300

    Auth Layout Sizing Fix

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,4 +1,4 @@
-import { Container, Typography, Box, Grid } from '@mui/material'
+import { Box, Grid } from '@mui/material'
 import { Link } from 'react-router-dom'
 import Logo from '../assets/logo/logo.png'
 
@@ -10,40 +10,53 @@ type AuthLayoutProps = {
 const AuthLayout = ({ children, imageSrc }: AuthLayoutProps) => {
   return (
     <>
-      <Box component='section' sx={{ bgcolor: '#f5f5f5' }}>
-        <Container
-          className='AuthContainer'
-          sx={{ py: 3, height: '100vh', bgcolor: '#fff' }}
-          maxWidth='lg'
-        >
-          <Grid container sx={{ height: '100%' }}>
-            <Grid item xs={12} md={6} sx={{ height: '100%' }}>
-              <Box height='100%'>
-                <Typography variant='h5'>
-                  <Link to='/'>
-                    <img src={Logo} alt='logo' width='100' />
-                  </Link>
-                </Typography>
-                <Box sx={{ m: 5 }}>{children}</Box>
-              </Box>
-            </Grid>
-            <Grid item xs={12} md={6} sx={{ height: '100%' }}>
-              <Box sx={{ width: '100%', height: '100%', overflow: 'hidden' }}>
-                <img
-                  src={imageSrc}
-                  alt='image'
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    objectFit: 'contain',
-                    display: 'block',
-                  }}
-                />
-              </Box>
-            </Grid>
-          </Grid>
-        </Container>
-      </Box>
+      <Grid component={'section'} container sx={{ height: '100vh', p: 3 }}>
+        <Grid item xs={12} md={6} sx={{ height: { xs: '50%', md: '100%' } }}>
+          <Box
+            height='100%'
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'start',
+              alignItems: 'start',
+            }}
+          >
+            <Link to='/'>
+              <img src={Logo} alt='logo' width='150' />
+            </Link>
+
+            <Box
+              sx={{
+                p: 6,
+                flexGrow: 1,
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'start',
+                maxWidth: '100%',
+              }}
+            >
+              {children}
+            </Box>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6} sx={{ height: { xs: '50%', md: '100%' } }}>
+          <Box sx={{ width: '100%', height: '100%' }}>
+            <img
+              src={imageSrc}
+              alt='image'
+              style={{
+                height: '100%',
+                width: '100%',
+                maxWidth: '100%',
+                borderRadius: '1rem',
+                objectFit: 'cover',
+                display: 'block',
+              }}
+            />
+          </Box>
+        </Grid>
+      </Grid>
     </>
   )
 }
