d071fd5cb3a898a701374537cb4a54bea93c7eba
abdalrahman <abdalrahmanshaip@gmail.com>
2024-05-31 13:13:01 +0300

    Fixed Hero component styles  (girds)

--- src/components/ui/DetailsPageHero.tsx
+++ src/components/ui/DetailsPageHero.tsx
@@ -1,52 +1,52 @@
-import { Grid } from '@mui/material'
-
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 
 const Hero = () => {
   return (
-    <Grid
-      container
-      mt={10}
-    >
-      <Grid
-        borderRadius={20}
-        md={8}
-        item
-      >
-        <img
-          style={{ borderRadius: '20px' }}
-          width={980}
-          src='https://s3-alpha-sig.figma.com/img/2c6d/fb26/9bc936a5b8286136ac5c292784bf42d5?Expires=1717977600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hV3-tuQeqnA985vf7uGRV4pUDFPVc8K2bzAuOuGlN9iyDkuuGMSFaUqd2ukDhKnbgkCC0AH4i6dF7g~rvib-XVJQVktU3Lh5PCj5epw4Es3DSxhjFYJdgRBw17FAmWsL9MOFvpkKnqD-QOR6m8Qm174Ny7jXMZcTE2Opz85viHd3ijyZNo~vMztvTIFAu1p9FE88m7yf8X4B80W~~HA8~bZjtVPvBUo4~GBrmFcsXry9upSjRB6NqCDjt3o-dEokzBt7vakdAEw82gGmdyXVhce6GvayQu4qMhUsxXu09RhB0kv5rB0E7y8pTMKkKPKqKFpqHxRyyWZZuoYTZrqktw__'
-          alt=''
-        />
-      </Grid>
-      <Grid md={4}>
-        <Grid
-          md={2}
-          item
-        >
+    <Box mt={10}>
+      <Grid container spacing={2}>
+        <Grid item md={8} sm={12}>
           <img
-            style={{ borderRadius: '20px' }}
-            width={500}
-            height={500}
-            src='https://s3-alpha-sig.figma.com/img/2b73/a665/f594704ec6796ef5e6c3603b7e47f06a?Expires=1717977600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qw0-MbwyhvPcr865mutd6ZZsT7k3DZG-tNGIvi2I39Sv2CouT~SGRNxEZz9gk0iAh~lfcRX~IHtRVjHpheTX6mRAPJy554p3xYPvOhTPFN~yP7GB2AM9T6TCQTSgayLWrvH4eLzIf31fNlV3hM7S67v0fd-abqyRb9uGstyt2KEEtPW4v1ZpFWxjLRYE7pXj3y3n0X54ACg9p0zg38WFnVX1XF-2g6ZzX5zpWW-dkb4huvvRX6tYbM8wS2Tap4yy-akulYCztI~PXQrlf8EeQaIoE4HKJzAm2k52AYKQ17ndm32AZXHd5nrheQ4cjOlechwGPHnniTGcKOaGxbd2hA__'
-            alt=''
+            style={{
+              borderRadius: '20px',
+              objectFit: 'cover',
+              width: '100%',
+              height: '100%'
+            }}
+            src='https://s3-alpha-sig.figma.com/img/2c6d/fb26/9bc936a5b8286136ac5c292784bf42d5?Expires=1717977600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hV3-tuQeqnA985vf7uGRV4pUDFPVc8K2bzAuOuGlN9iyDkuuGMSFaUqd2ukDhKnbgkCC0AH4i6dF7g~rvib-XVJQVktU3Lh5PCj5epw4Es3DSxhjFYJdgRBw17FAmWsL9MOFvpkKnqD-QOR6m8Qm174Ny7jXMZcTE2Opz85viHd3ijyZNo~vMztvTIFAu1p9FE88m7yf8X4B80W~~HA8~bZjtVPvBUo4~GBrmFcsXry9upSjRB6NqCDjt3o-dEokzBt7vakdAEw82gGmdyXVhce6GvayQu4qMhUsxXu09RhB0kv5rB0E7y8pTMKkKPKqKFpqHxRyyWZZuoYTZrqktw__'
+            alt='Main'
           />
         </Grid>
-        <Grid
-          md={2}
-          item
-        >
-          <img
-            style={{ borderRadius: '20px' }}
-            width={500}
-            height={460}
-            src='https://s3-alpha-sig.figma.com/img/bf47/c1e1/2323739f3ca061db912f14b9dde0b461?Expires=1717977600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EJNoQiknkfiuavNUV2hIsqcAxWEqEIKTUENbble6~w4SRboJmd9AW72-TpmpWHmpDR59Qj9VaqyHUatIKYuxf2ETs18X6f6Q2eFftHY9Czx-6fJs76OLcateUMjYON484ENbz4VTdwRr9o-G9HujCIpYGnEcUkEdeXW0asp7vRmZj3CtsKZrCHKKc81OBbGixM3eXt3V6G02YgPZiR61Rt6k01SYyrollLq5ijehXww-aLya67TsoiUqgCxvBcPP~cFkMAOJ-~yVaJp67EAJ7tPLbAO6HBp1Bjl57YVTXcgXD60XUGmqHumAGwySBtJVMYNpsH3eXCbQnpwdcjBcoQ__'
-            alt=''
-          />
+        <Grid item container md={4} sm={12} spacing={2}>
+          <Grid item xs={6} sm={6} md={12}>
+            <img
+              style={{
+                borderRadius: '20px',
+                objectFit: 'cover',
+                width: '100%',
+                height: '100%'
+              }}
+              src='https://s3-alpha-sig.figma.com/img/2b73/a665/f594704ec6796ef5e6c3603b7e47f06a?Expires=1717977600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qw0-MbwyhvPcr865mutd6ZZsT7k3DZG-tNGIvi2I39Sv2CouT~SGRNxEZz9gk0iAh~lfcRX~IHtRVjHpheTX6mRAPJy554p3xYPvOhTPFN~yP7GB2AM9T6TCQTSgayLWrvH4eLzIf31fNlV3hM7S67v0fd-abqyRb9uGstyt2KEEtPW4v1ZpFWxjLRYE7pXj3y3n0X54ACg9p0zg38WFnVX1XF-2g6ZzX5zpWW-dkb4huvvRX6tYbM8wS2Tap4yy-akulYCztI~PXQrlf8EeQaIoE4HKJzAm2k52AYKQ17ndm32AZXHd5nrheQ4cjOlechwGPHnniTGcKOaGxbd2hA__'
+              alt='First'
+            />
+          </Grid>
+          <Grid item xs={6} sm={6} md={12}>
+            <img
+              style={{
+                borderRadius: '20px',
+                objectFit: 'cover',
+                width: '100%',
+                height: '100%'
+              }}
+              src='https://s3-alpha-sig.figma.com/img/bf47/c1e1/2323739f3ca061db912f14b9dde0b461?Expires=1717977600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EJNoQiknkfiuavNUV2hIsqcAxWEqEIKTUENbble6~w4SRboJmd9AW72-TpmpWHmpDR59Qj9VaqyHUatIKYuxf2ETs18X6f6Q2eFftHY9Czx-6fJs76OLcateUMjYON484ENbz4VTdwRr9o-G9HujCIpYGnEcUkEdeXW0asp7vRmZj3CtsKZrCHKKc81OBbGixM3eXt3V6G02YgPZiR61Rt6k01SYyrollLq5ijehXww-aLya67TsoiUqgCxvBcPP~cFkMAOJ-~yVaJp67EAJ7tPLbAO6HBp1Bjl57YVTXcgXD60XUGmqHumAGwySBtJVMYNpsH3eXCbQnpwdcjBcoQ__'
+              alt='Second'
+            />
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
-  )
-}
+    </Box>
+  );
+};
 
-export default Hero
+export default Hero;
