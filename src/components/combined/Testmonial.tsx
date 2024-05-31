8e51da9e52d02d93c8432acd2fb9b314ab56bd02
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-05-31 15:00:51 +0300

    Testmonial Data Sizing and Spacing fix

--- src/components/combined/Testmonial.tsx
+++ src/components/combined/Testmonial.tsx
@@ -1,13 +1,20 @@
-import { Stack } from "@mui/material";
-import { TestmonialsData, TestmonialsImage } from "../ui";
-
+import { Box, Stack } from '@mui/material'
+import { TestmonialsData, TestmonialsImage } from '../ui'
 
 const Testmonial = () => {
   return (
     <>
-      <Stack direction={"row"} spacing={5}>
+      <Stack direction={'row'} spacing={5}>
         <TestmonialsImage />
-        <TestmonialsData />
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <TestmonialsData />
+        </Box>
       </Stack>
     </>
   )
