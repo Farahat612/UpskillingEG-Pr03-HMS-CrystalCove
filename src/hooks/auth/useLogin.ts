ad63c781a8d83995f54bb75a8ee0c62d78b9e770
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-06-01 08:46:19 +0300

    Auth Context
    - created auth context, useAuthcontext custom hook
    - wrapped App with auth provider
    - used auth context in login hook to store the login data

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -1,10 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import { apiPublic } from '../../utils/api'
 import { FormData } from '../../types'
 import { toast } from 'sonner'
+import { useAuthContext } from '../../contexts/global/AuthContext'
 const useLogin = ({ userType }: { userType: 'portal' | 'admin' }) => {
+  const { login } = useAuthContext()
+
   const [passwordVisible, setPasswordVisible] = useState(false)
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible)
@@ -22,8 +26,10 @@ const useLogin = ({ userType }: { userType: 'portal' | 'admin' }) => {
       const res = await apiPublic.post(`/${userType}/users/login`, data)
       localStorage.setItem('token', res.data.data.token)
       toast.success(res.data.message)
+      // storing token and usertype in local storage
+      const signinData = res.data.data
+      login(signinData.token, userType, signinData.role)
       navigate('/')
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
       toast.error('Invalid Credentials!')
     }
