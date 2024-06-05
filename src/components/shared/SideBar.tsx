e0486a6386eeb74a1742e68fc162c8cb3dddd5c0
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-06-05 12:43:02 +0300

    Side Bar routing issue fixed

--- src/components/shared/SideBar.tsx
+++ src/components/shared/SideBar.tsx
@@ -1,22 +1,22 @@
-import Box from "@mui/material/Box";
-import List from "@mui/material/List";
-import IconButton from "@mui/material/IconButton";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import { useState } from "react";
-import HomeIcon from "@mui/icons-material/Home";
-import PeopleIcon from "@mui/icons-material/People";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import BookOnlineIcon from "@mui/icons-material/BookOnline";
-import LockIcon from "@mui/icons-material/Lock";
-import LogoutIcon from "@mui/icons-material/Logout";
-import { Link } from "react-router-dom";
-import { useAuthContext } from "../../contexts/global/AuthContext";
+import Box from '@mui/material/Box'
+import List from '@mui/material/List'
+import IconButton from '@mui/material/IconButton'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import { useState } from 'react'
+import HomeIcon from '@mui/icons-material/Home'
+import PeopleIcon from '@mui/icons-material/People'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth'
+import BookOnlineIcon from '@mui/icons-material/BookOnline'
+import LockIcon from '@mui/icons-material/Lock'
+import LogoutIcon from '@mui/icons-material/Logout'
+import { Link } from 'react-router-dom'
+import { useAuthContext } from '../../contexts/global/AuthContext'
 
 import {
   listItemStyles,
@@ -24,46 +24,45 @@ import {
   listItemIconStyles,
   DrawerHeader,
   Drawer,
-} from "../styled/Sidebarstyled";
-
+} from '../styled/Sidebarstyled'
 
 const SideBar = () => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false)
 
   const handleDrawerToggle = () => {
-    setOpen(!open);
-  };
-  const {
-      logout
-  } = useAuthContext()
+    setOpen(!open)
+  }
+  const { logout } = useAuthContext()
   return (
-    <Box sx={{ display: "flex", bgcolor: "#203FC7" }}>
+    <Box sx={{ display: 'flex', bgcolor: '#203FC7' }}>
       <Drawer
-        PaperProps={{ sx: { bgcolor: "#203FC7", height: "100vh" } }}
-        variant="permanent"
-        open={open}>
+        PaperProps={{ sx: { bgcolor: '#203FC7', height: '100vh' } }}
+        variant='permanent'
+        open={open}
+      >
         <DrawerHeader>
           <IconButton
             sx={{
-              color: "#fff",
-              bgcolor: "#f484ad",
-              "&:hover": {
-                bgcolor: "#d4698f",
+              color: '#fff',
+              bgcolor: '#f484ad',
+              '&:hover': {
+                bgcolor: '#d4698f',
               },
             }}
-            onClick={handleDrawerToggle}>
+            onClick={handleDrawerToggle}
+          >
             {open ? <ChevronRightIcon /> : <ChevronLeftIcon />}
           </IconButton>
         </DrawerHeader>
         <List>
           {[
-            { text: "Home", icon: <HomeIcon /> },
-            { text: "Users", icon: <PeopleIcon /> },
-            { text: "Rooms", icon: <DashboardIcon /> },
-            { text: "Ads", icon: <CalendarMonthIcon /> },
-            { text: "Bookings", icon: <BookOnlineIcon /> },
+            { text: 'Home', icon: <HomeIcon /> },
+            { text: 'Users', icon: <PeopleIcon /> },
+            { text: 'Rooms', icon: <DashboardIcon /> },
+            { text: 'Ads', icon: <CalendarMonthIcon /> },
+            { text: 'Bookings', icon: <BookOnlineIcon /> },
           ].map(({ text, icon }) => (
-            <Link to={text} key={text}>
+            <Link to={`/admin/${text}`} key={text}>
               <ListItem key={text} disablePadding sx={listItemButtonStyles}>
                 <ListItemButton sx={listItemStyles}>
                   <ListItemIcon sx={listItemIconStyles}>{icon}</ListItemIcon>
@@ -79,7 +78,7 @@ const SideBar = () => {
                 <LockIcon />
               </ListItemIcon>
               <ListItemText
-                primary="Change Password"
+                primary='Change Password'
                 sx={{ opacity: open ? 1 : 0 }}
               />
             </ListItemButton>
@@ -89,14 +88,14 @@ const SideBar = () => {
               <ListItemIcon sx={listItemIconStyles}>
                 <LogoutIcon />
               </ListItemIcon>
-              <ListItemText primary="Logout" sx={{ opacity: open ? 1 : 0 }} />
+              <ListItemText primary='Logout' sx={{ opacity: open ? 1 : 0 }} />
             </ListItemButton>
           </ListItem>
         </List>
         <List></List>
       </Drawer>
     </Box>
-  );
-};
+  )
+}
 
-export default SideBar;
+export default SideBar
