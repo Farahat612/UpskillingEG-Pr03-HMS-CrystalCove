8863ff1165d59d72c25acc9f4c228e889555b1f7
Saje Mohammed <saje.dev963@gmail.com>
2024-06-12 00:31:54 +0300

    Show all rooms & Show Fav's Rooms

--- src/pages/User/Favorites.tsx
+++ src/pages/User/Favorites.tsx
@@ -1,49 +1,36 @@
 import { Container, Grid, Typography } from "@mui/material";
 import { Footer, Header, Navbar } from "../../components/shared";
-import ADSRoom from "../../components/ui/ADSRoomCard";
-
-
+import FavRoomsCards from "../../components/ui/FavRoomsCard";
 
 const Favorites = () => {
   return (
     <>
-        <Navbar/>
-            <Container>
-                <Header headerName={'Your Favorites'} subtitleHeader={''}/>
-                <Typography
-                    mt={3}
-                    variant='h6'
-                    color={'#152C5B'}
-                    >
-                        Your Rooms
-                </Typography>
-                <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }} mt={1} >
-                    <Grid item xs={12} sm={6} md={4}>
-                        <ADSRoom/>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <ADSRoom/>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <ADSRoom/>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <ADSRoom/>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <ADSRoom/>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <ADSRoom/>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <ADSRoom/>
-                    </Grid>
-                </Grid>
-            </Container>
-            <Footer/>
+      <Navbar />
+      <Container>
+        <Header headerName={"Your Favorites"} subtitleHeader={""} />
+        <Typography mt={3} variant="h6" color={"#152C5B"}>
+          Your Favorites Rooms
+        </Typography>
+        <Grid
+          container
+          rowSpacing={2}
+          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          mt={1}
+        >
+          <Grid item xs={12} sm={6} md={4}>
+            <FavRoomsCards />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <FavRoomsCards />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <FavRoomsCards />
+          </Grid>
+        </Grid>
+      </Container>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Favorites
+export default Favorites;
