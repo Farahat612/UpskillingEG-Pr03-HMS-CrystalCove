f28a02c160238f47d0928c21458d603fca76759b
abdalrahman <abdalrahmanshaip@gmail.com>
2024-06-23 15:20:16 +0300

    Remove css file and fixed the style with mui not css

--- src/components/routing/NotFound.tsx
+++ src/components/routing/NotFound.tsx
@@ -1,28 +1,27 @@
 import { Box, Button, Typography } from "@mui/material";
 import notFound from "../../assets/images/8961448_3973481.jpg";
 import { useNavigate } from "react-router-dom";
-import "../styled/NoutFound.css";
 const NotFound = () => {
   const navigate = useNavigate();
   const handleClick = () => navigate("/");
   return (
-    <>
-      <Box>
-        <Box className="flex">
-          <img src={notFound} alt="logo" className="NotFoundImg" />
+    <Box display={'flex'} justifyContent={"center"} alignContent={'center'}>
+      <Box  width={700} height={450} >
+        <Box >
+          <img src={notFound} alt="logo" width={'100%'} height={'100%'} />
         </Box>
-        <Box>
-          <Typography variant="h1">Oops!</Typography>
-          <Typography variant="h4">404 - PAGE NOT FOUND.</Typography>
-          <Typography variant="subtitle1">
+        <Box textAlign={'center'}>
+          <Typography variant="h1" color={'primary.main'} textAlign={'center'} fontWeight={700}>Oops!</Typography>
+          <Typography variant="h4" fontWeight={700} my={2}>404 - PAGE NOT FOUND.</Typography>
+          <Typography variant="h6" color={'textLight.main'}>
             This Page doesn't exist or was removed! We suggest you back to home.
           </Typography>
-          <Box className="flex">
+          <Box mt={2}>
             <Button onClick={handleClick} variant="contained" >Back To Home</Button>
           </Box>
         </Box>
       </Box>
-    </>
+    </Box >
   );
 };
 
