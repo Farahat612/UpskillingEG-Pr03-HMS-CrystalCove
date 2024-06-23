fdd1e9ce0cde771ad1223bfcaa08c19b89eb6b7c
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-06-23 12:42:45 +0300

    Refactoring All Ads Page

--- src/components/styled/RoomImage.styled.ts
+++ src/components/styled/RoomImage.styled.ts
@@ -3,6 +3,7 @@ import styled from '@emotion/styled'
 export const RoomImage = styled.img`
   object-fit: cover;
   width: 100%;
+  max-width: 300px;
   aspect-ratio: 1/1;
   border-radius: 15px;
 `
--- src/pages/Public/AllAds.tsx
+++ src/pages/Public/AllAds.tsx
@@ -1,93 +1,71 @@
-import { useState, useEffect } from "react";
-import { Container, Grid, Typography, Box, IconButton } from "@mui/material";
-import { Footer, Header, Navbar } from "../../components/shared";
-import { apiPublic } from "../../utils/api";
-import { RoomImage } from "./../../components/styled/RoomImage.styled";
+import { Favorite, Visibility } from '@mui/icons-material'
+import { Box, Container, Grid, IconButton, Typography } from '@mui/material'
+import { Footer, Header, Navbar } from '../../components/shared'
+import { useFetchPublicData } from '../../hooks/portal/useFetchPublicData'
+import { Ad } from '../../types'
 import {
   BadgedBox,
   IconsBox,
   LayerBox,
   RoomName,
-} from "./../../components/styled/RoomBoxStyle";
-import { Favorite, Visibility } from "@mui/icons-material";
-
-interface Room {
-  _id: string;
-  price: number;
-  discount: number;
-  roomNumber: string;
-  images: string[];
-  facilities: string[];
-}
-
-interface Ad {
-  _id: string;
-  isActive: boolean;
-  room: Room;
-}
+} from './../../components/styled/RoomBoxStyle'
+import { RoomImage } from './../../components/styled/RoomImage.styled'
 
 const AllAds = () => {
-  const [ads, setAds] = useState<Ad[]>([]);
-
-  const getItem = async () => {
-    try {
-      const { data } = await apiPublic.get(`/portal/ads`);
-      setAds(data.data.ads);
-    } catch (err) {
-      console.error("Error fetching data", err);
-    }
-  };
-
-  useEffect(() => {
-    getItem();
-  }, []);
+  const { data: ads, loading } = useFetchPublicData<Ad[]>('ads', 'ads')
 
   return (
     <>
       <Navbar />
       <Container>
-        <Header headerName={"Explore ALL Ads"} subtitleHeader={""} />
-        <Typography mt={3} variant="h6" color={"#152C5B"}>
+        <Header headerName={'Explore ALL Ads'} subtitleHeader={''} />
+        <Typography mt={3} variant='h6' color={'#152C5B'}>
           All Ads
         </Typography>
         <Grid
           container
           rowSpacing={2}
           columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-          mt={1}>
-          {ads.map((item) => (
-            <Grid item xs={12} sm={6} md={4} key={item.room._id}>
-              <Box
-                sx={{
-                  position: "relative",
-                  borderRadius: "15px",
-                  width: "fit-content",
-                  cursor: "pointer",
-                }}>
-                <RoomImage src={item.room.images[0]} alt="RoomPicture" />
-                <BadgedBox>{item.room.discount}</BadgedBox>
-                <RoomName>
-                  <Typography variant="h6">{item.room.roomNumber}</Typography>
-                  {/* <Typography>{item.room.facilities}</Typography> */}
-                </RoomName>
-                <LayerBox>
-                  <IconsBox>
-                    <IconButton sx={{ color: "#FFFFFF" }}>
-                      <Favorite />
-                    </IconButton>
-                    <IconButton sx={{ color: "#FFFFFF" }}>
-                      <Visibility />
-                    </IconButton>
-                  </IconsBox>
-                </LayerBox>
-              </Box>
-            </Grid>
-          ))}
+          mt={1}
+        >
+          {!loading ? (
+            ads.map((item) => (
+              <Grid item xs={12} sm={6} md={4} key={item.room._id}>
+                <Box
+                  sx={{
+                    position: 'relative',
+                    borderRadius: '15px',
+                    width: 'fit-content',
+                    cursor: 'pointer',
+                  }}
+                >
+                  <RoomImage src={item.room.images[0]} alt='RoomPicture' />
+                  <BadgedBox>{item.room.discount}</BadgedBox>
+                  <RoomName>
+                    <Typography variant='h6'>{item.room.roomNumber}</Typography>
+                    {/* <Typography>{item.room.facilities}</Typography> */}
+                  </RoomName>
+                  <LayerBox>
+                    <IconsBox>
+                      <IconButton sx={{ color: '#FFFFFF' }}>
+                        <Favorite />
+                      </IconButton>
+                      <IconButton sx={{ color: '#FFFFFF' }}>
+                        <Visibility />
+                      </IconButton>
+                    </IconsBox>
+                  </LayerBox>
+                </Box>
+              </Grid>
+            ))
+          ) : (
+            <div>Loading...</div>
+          )}
         </Grid>
       </Container>
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default AllAds;
+export default AllAds
