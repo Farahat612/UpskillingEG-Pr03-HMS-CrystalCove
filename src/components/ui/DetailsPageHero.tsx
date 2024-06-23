404f3c281bc75cb513b4204bde87f6103e3237c8
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-06-23 13:59:56 +0300

    Preparing for refactoring

--- src/components/ui/DetailsPageHero.tsx
+++ src/components/ui/DetailsPageHero.tsx
@@ -1,36 +1,30 @@
-import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
-import { DetailsPageImage } from '../styled/img'
-import useFetchDetails from '../../hooks/portal/useFetchDetails'
-import { useParams } from 'react-router-dom'
+import Grid from '@mui/material/Grid'
+import { Room } from '../../types'
 import { LoadingPage } from '../shared'
+import { DetailsPageImage } from '../styled/img'
 
-const DetailsHero = () => {
-  const { id } = useParams()
-  const { data , loadingDone } = useFetchDetails(`rooms/${id}`, 'room')
+interface DetailsHeroProps {
+  item: Room
+  loading: boolean
+}
 
+const DetailsHero = ({ item, loading }: DetailsHeroProps) => {
   return (
     <>
-      {!loadingDone ? (
+      {!loading ? (
         <Box mt={10}>
-          <Grid
-            container
-            spacing={2}
-          >
-            {data.images.map((img, index) => {
+          <Grid container spacing={2}>
+            {item.images.map((img, index) => {
               return (
                 <Grid
                   key={index}
                   item
                   maxHeight={600}
-                  md={data.images.length >= 2 ? 6 : 12}
+                  md={item.images.length >= 2 ? 6 : 12}
                   sm={12}
                 >
-                  <DetailsPageImage
-                    key={index + 1}
-                    src={`${img}`}
-                    alt='Main'
-                  />
+                  <DetailsPageImage key={index + 1} src={`${img}`} alt='Main' />
                 </Grid>
               )
             })}
--- src/pages/Public/RoomDetails.tsx
+++ src/pages/Public/RoomDetails.tsx
@@ -1,14 +1,22 @@
 import { Container } from '@mui/material'
 import { Navbar, Header, Footer } from '../../components/shared'
 import { AboutDetails, DetailsHero, RatingDetails } from '../../components/ui'
+import { useFetchPublicData } from '../../hooks/portal/useFetchPublicData'
+import { Room } from '../../types'
+import { useParams } from 'react-router-dom'
 
 const RoomDetails = () => {
+  const { id } = useParams()
+  const { data: item, loading } = useFetchPublicData<Room>(
+    `rooms/${id}`,
+    'room'
+  )
   return (
     <>
-      <Navbar  />
+      <Navbar />
       <Container maxWidth='xl' sx={{ margin: 'auto' }}>
         <Header headerName='Village Angga' subtitleHeader='Bogor, Indonesia' />
-        <DetailsHero />
+        <DetailsHero item={item as Room} loading={loading} />
         <AboutDetails />
         <RatingDetails />
       </Container>
