19b786d53e093f12b11a5e844fe710ee0a0fa335
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-06-23 12:28:12 +0300

    Create useFetchPublicData.ts

---
+++ src/hooks/portal/useFetchPublicData.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { apiPublic } from '../../utils/api'
+import { useEffect, useState } from 'react'
+
+export const useFetchPublicData = <T>(endpoint: string, responseListName: string) => {
+  const [data, setData] = useState<T | []>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await apiPublic.get(`/portal/${endpoint}`)
+        setData(response.data.data[responseListName])
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [endpoint, responseListName])
+
+  return {
+    data,
+    loading,
+  }
+}
