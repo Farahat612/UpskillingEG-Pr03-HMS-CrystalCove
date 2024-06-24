5548bcc242efb2142a8abfe22dbd8612e6e97479
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-06-24 12:21:15 +0300

    Refactoring LargeRoom props

--- src/components/ui/LargeRoom.tsx
+++ src/components/ui/LargeRoom.tsx
@@ -1,45 +1,43 @@
-import { Box, IconButton, Typography } from '@mui/material'
-import { RoomImage } from '../styled/RoomImage.styled'
-import { BadgedBox, IconsBox, LayerBox } from '../styled/RoomBoxStyle'
-import { Favorite, Visibility } from '@mui/icons-material'
+import { Favorite } from '@mui/icons-material';
+import { Box, IconButton, Typography } from '@mui/material';
+import { Ad } from '../../types';
+import { BadgedBox, IconsBox, LayerBox } from '../styled/RoomBoxStyle';
+import { RoomImage } from '../styled/RoomImage.styled';
 interface LargeRoomProps {
-  roomPicture?: string;
-  price?: number;
-  title?: string;
-  location?: string;
+
+  item: Ad;
 }
 
-export default function LargeRoom({ roomPicture, price, title, location }: LargeRoomProps) {
+export default function LargeRoom({item }: LargeRoomProps) {
   return (
     <Box>
       <Box
         sx={{
-          position: "relative",
-          borderRadius: "15px",
-          width: "inherit",
-          cursor: "pointer",
-        }}>
-        <RoomImage src={roomPicture} alt="RoomPicture" />
-        <BadgedBox>{price} per night</BadgedBox>
+          position: 'relative',
+          borderRadius: '15px',
+          width: 'inherit',
+          cursor: 'pointer',
+        }}
+      >
+        <RoomImage src={item.room.images[0]} alt='RoomPicture' />
+        <BadgedBox>{item.price} per night</BadgedBox>
         <LayerBox>
           <IconsBox>
-            <IconButton sx={{ color: "#FFFFFF" }}>
+            <IconButton sx={{ color: '#FFFFFF' }}>
               <Favorite />
             </IconButton>
-            <IconButton sx={{ color: "#FFFFFF" }}>
+            {/* <IconButton sx={{ color: "#FFFFFF" }}>
               <Visibility />
-            </IconButton>
+            </IconButton> */}
           </IconsBox>
         </LayerBox>
       </Box>
-      <Box fontSize="clamp(.5rem, 3.4vw, 1rem)">
-        <Typography variant="h6" color="#152C5B">
-          {title} 
+      <Box fontSize='clamp(.5rem, 3.4vw, 1rem)'>
+        <Typography variant='h6' color='#152C5B'>
+          {item.room.roomNumber}
         </Typography>
-        <Typography color="#B0B0B0" fontSize="15px">
-          {location} 
-        </Typography>
+        
       </Box>
     </Box>
-  );
+  )
 }
