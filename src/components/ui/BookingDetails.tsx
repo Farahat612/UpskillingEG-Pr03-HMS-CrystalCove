3e23a2e29b7fc9964f71a72c9eb36dd6459aa8ff
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-08-13 09:12:43 +0300

    Added needed states in details booking form

--- src/components/ui/BookingDetails.tsx
+++ src/components/ui/BookingDetails.tsx
@@ -1,12 +1,14 @@
 import { Box, Button, Paper, Stack, Typography } from '@mui/material'
 import { DatePick, CapacityButtonGroup } from '../forms_utilities'
+import { useState } from 'react'
+import dayjs, { Dayjs } from 'dayjs'
 
 const BookingDetails = () => {
+  const [capacity, setCapacity] = useState(2)
+  const [date, setDate] = useState<Dayjs>(dayjs())
+
   return (
-    <Paper
-      elevation={1}
-      sx={{ borderRadius: 3, mt: 'auto' }}
-    >
+    <Paper elevation={1} sx={{ borderRadius: 3, mt: 'auto' }}>
       <Box sx={{ m: 5 }}>
         <Box>
           <Typography
@@ -17,33 +19,22 @@ const BookingDetails = () => {
           >
             Start Booking
           </Typography>
-          <Stack
-            direction='column'
-            spacing={4}
-          >
-            <Typography
-              variant='subtitle1'
-              gutterBottom
-              color={`primary`}
-            >
+          <Stack direction='column' spacing={4}>
+            <Typography variant='subtitle1' gutterBottom color={`primary`}>
               Pick a Date
             </Typography>
-            <DatePick />
-            <CapacityButtonGroup />
-            <Box
-              display={'flex'}
-              gap={0.6}
-              flexWrap={'wrap'}
-            >
+            <DatePick date={date} setDate={setDate} />
+            <CapacityButtonGroup
+              capacity={capacity}
+              setCapacity={setCapacity}
+            />
+            <Box display={'flex'} gap={0.6} flexWrap={'wrap'}>
               <Typography color={`textLight.main`}>You will pay</Typography>
               <Typography color={`primary.main`}>$480 USD</Typography>
               <Typography color={`textLight.main`}>per</Typography>
               <Typography color={`primary.main`}>2 Presone</Typography>
             </Box>
-            <Button
-              variant='contained'
-              color={`primary`}
-            >
+            <Button variant='contained' color={`primary`}>
               Continue Book
             </Button>
           </Stack>
