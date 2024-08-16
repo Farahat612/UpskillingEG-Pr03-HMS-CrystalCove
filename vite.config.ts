2105d67450aef1faf2302eaa11ad03d13e90fa7b
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-08-16 08:32:23 +0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    define: {
-      'process.env.API_BASE_URL': JSON.stringify(env.API_BASE_URL),
-      'process.env.API_STATIC_URL': JSON.stringify(env.API_STATIC_URL),
-    },
-    plugins: [react()],
-  }
+export default defineConfig({
+  plugins: [react()],
 })
