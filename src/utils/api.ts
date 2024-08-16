b939a4fe519a41ad74389d8731507f1d24d4b528
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-08-16 08:33:16 +0300

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_URL as string
-const staticURL = import.meta.env.VITE_STATIC_URL as string
+const baseURL = import.meta.env.VITE_API_BASE_URL as string
+const staticURL = import.meta.env.VITE_API_STATIC_URL as string
 
 const apiPublic = axios.create({
   baseURL,
